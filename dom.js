964d0fc3186e3dfb5b139a46f2b177bf4153ef3d
nikita3006 <nikitanegi3011@gmail.com>
2023-04-24 21:51:01 -0700

    this is my dom work

---
+++ dom.js
@@ -0,0 +1,9 @@
+document.getElementById("main-header").style.border = "thick solid black";
+document.querySelector("h2").style.fontWeight = "bold";
+document.querySelector("h2").style.color = "green";
+
+
+
+
+
+
